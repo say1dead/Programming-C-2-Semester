442c5900e4e87bc15e66f9f13f5898828cc0a65d
say1dead <107905872+say1dead@users.noreply.github.com>
2023-02-06 22:01:27 +0300

    Create lab1.h

---
+++ lab1.h
@@ -0,0 +1,20 @@
+//
+// Created by kochu on 06.02.2023.
+//
+
+#ifndef UNTITLED28_LAB1_H
+#define UNTITLED28_LAB1_H
+#include <iostream>
+using namespace std;
+
+//2
+void summ(float& a, float& b);
+//5
+void not_real(float& a);
+
+//10
+void complex_num(float& a, float& b);
+
+//13
+void square(float& x1, float& y1, float& s, float& a, float& b);
+#endif //UNTITLED28_LAB1_H
