63a012a4f307d73b3f0d34a891e7e9948b92d530
say1dead <107905872+say1dead@users.noreply.github.com>
2023-02-09 17:15:33 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
 //2
 void summ_ref(float& a, float& b, float& c){
     c = a + b;
@@ -54,100 +55,125 @@ void test_without_real(){
 }
 
 //10
-void complex_num_ref(float& a, float& b){//a - re, b - im
-    b = -b;
+
+struct complex_ref{
+    float real;
+    float imagine;
+    float imagine_after;
+};
+
+void complex_num_ref(complex_ref &num){//a - re, b - im
+    num.imagine_after = -num.imagine;
 }
 
-void  complex_num_point(float* a, float* b){
-    *b = -*b;
+void complex_num_point(complex_ref *num){
+    num->imagine_after = -num->imagine;
 }
+
 
 //test complex_num
 void test_complex_num(){
-    float a = 2;
-    float b = -1;
-    complex_num_ref(a, b);
-    float b_rev = -1;
-    complex_num_point(&a, &b_rev);
-    if ((b == 1) and (b_rev == 1)){
+    complex_ref t_1;
+    t_1.real = 2;
+    t_1.imagine = 1;
+    complex_num_ref(t_1);
+    complex_ref t_2;
+    t_2.real = 2;
+    t_2.imagine = 1;
+    complex_num_point(&t_2);
+    if ((t_1.imagine_after == -1) and (t_2.imagine_after == -1)){
         cout << "\nTEST THIRD MODULE - THIRD MODULE CORRECT";
     }
     else{
         cout << "\nTEST THIRD MODULE - THIRD MODULE NOT CORRECT";
     }
 }
-
 //13
-void square_ref(float& x1, float& y1, float& s, float& a, float& b, float& x2, float& x3, float&x4, float& y2, float& y3, float& y4){
-    x2 = x1 + s;
-    y2 = y1;
-    x3 = x2;
-    y3 = y2 - s;
-    x4 = x1;
-    y4 = y3;
-    
+struct square{
+    float x1;
+    float x2;
+    float x3;
+    float x4;
+    float y1;
+    float y2;
+    float y3;
+    float y4;
+    float s;
+    float k1;
+    float k2;
+};
+
+void square_ref(square &pt){
+    pt.x2 = pt.x1 + pt.s;
+    pt.y2 = pt.y1;
+    pt.x3 = pt.x2;
+    pt.y3 = pt.y2 - pt.s;
+    pt.x4 = pt.x1;
+    pt.y4 = pt.y3;
+
     //вектор аб направлен из правого верхнего угла (точка х2)
     float a1, b1;//координаты вектора
-    a1 = a + x2;
-    b1 = b + y2;
+    a1 = pt.k1 + pt.x2;
+    b1 = pt.k2 + pt.y2;
 
-    x1 = x1 + a1;
-    y1 = y1 + b1;
-    x2 = x2 + a1;
-    y2 = y2 + b1;
-    x3 = x3 + a1;
-    y3 = y3 + b1;
-    x4 = x4 + a1;
-    y4 = y4 + b1;
+    pt.x1 = pt.x1 + a1;
+    pt.y1 = pt.y1 + b1;
+    pt.x2 = pt.x2 + a1;
+    pt.y2 = pt.y2 + b1;
+    pt.x3 = pt.x3 + a1;
+    pt.y3 = pt.y3 + b1;
+    pt.x4 = pt.x4 + a1;
+    pt.y4 = pt.y4 + b1;
 }
 
-void square_point(float* x1, float* y1, float* s, float* a, float* b, float* x2, float* x3, float* x4, float* y2, float* y3, float* y4){
-    *x2 = *x1 + *s;
-    *y2 = *y1;
-    *x3 = *x2;
-    *y3 = *y2 - *s;
-    *x4 = *x1;
-    *y4 = *y3;
+void square_point(square *pt){
+    pt->x2 = pt->x1 + pt->s;
+    pt->y2 = pt->y1;
+    pt->x3 = pt->x2;
+    pt->y3 = pt->y2 - pt->s;
+    pt->x4 = pt->x1;
+    pt->y4 = pt->y3;
 
     //вектор аб направлен из правого верхнего угла (точка х2)
     float a1, b1;//координаты вектора
-    a1 = *a + *x2;
-    b1 = *b + *y2;
+    a1 = pt->k1 + pt->x2;
+    b1 = pt->k2 + pt->y2;
 
-    *x1 = *x1 + a1;
-    *y1 = *y1 + b1;
-    *x2 = *x2 + a1;
-    *y2 = *y2 + b1;
-    *x3 = *x3 + a1;
-    *y3 = *y3 + b1;
-    *x4 = *x4 + a1;
-    *y4 = *y4 + b1;
+    pt->x1 = pt->x1 + a1;
+    pt->y1 = pt->y1 + b1;
+    pt->x2 = pt->x2 + a1;
+    pt->y2 = pt->y2 + b1;
+    pt->x3 = pt->x3 + a1;
+    pt->y3 = pt->y3 + b1;
+    pt->x4 = pt->x4 + a1;
+    pt->y4 = pt->y4 + b1;
 }
 
 //test square
 void test_square(){
-    float x1 = 5;
-    float y1 = 5;
-    float s = 5;
-    float a = 5;
-    float b = 5;
-    float x2, x3, x4;
-    float y2, y3, y4;
-    square_ref(x1, y1, s, a, b, x2, x3, x4, y2, y3, y4);
-    float x1_t = x1;
-    float y1_t = y1;
-    float x2_t = x2;
-    float x3_t = x3;
-    float x4_t = x4;
-    float y2_t = y2;
-    float y3_t = y3;
-    float y4_t = y4;
-    square_point(&x1, &y1, &s, &a, &b, &x2, &x3, &x4, &y2, &y3, &y4);
-    if ((x1 == 20) and (x1_t == 20) and (y1 == 15) and (y1_t == 15) and (x2 == 25) and (x2_t == 25) and (y2 == 15) and (y2_t == 15) and (x3 == 25) and (x3_t == 25) and (y3 == 10) and (y3_t == 10) and (x4 == 20) and (x4_t == 20) and (y4 == 10) and (y4_t == 10)){
-        cout << "\nTEST FOURTH MODULE - FOURTH MODULE CORRECT";
+    square t_1;
+    t_1.x1 = 5;
+    t_1.y1 = 5;
+    t_1.s = 5;
+    t_1.k1 = 5;
+    t_1.k2 = 5;
+    square_ref(t_1);
+    square t_2;
+    t_2.x1 = t_1.x1;
+    t_2.y1 = t_1.y1;
+    t_2.x2 = t_1.x2;
+    t_2.x3 = t_1.x3;
+    t_2.x4 = t_1.x4;
+    t_2.y2 = t_1.y2;
+    t_2.y3 = t_1.y3;
+    t_2.y4 = t_1.y4;
+    square_point(&t_2);
+    if ((t_1.x1 == 20) and (t_2.x1 == 20) and (t_1.x2 == 25) and (t_2.x2 == 25) and (t_1.x3 == 25) and (t_2.x3 == 25) and (t_1.x4 == 20) and (t_2.x4 == 20)){
+        if((t_1.y1 == 15) and (t_2.y1 == 15) and (t_1.y2 == 15) and (t_2.y2 == 15) and (t_1.y3 == 10) and (t_2.y3 == 10) and (t_1.y4 == 10) and (t_2.y4 == 10)){
+            cout << "\nTEST FOURTH MODULE -  FOURTH MODULE CORRECT";
+        }
     }
     else{
         cout << "\nTEST FOURTH MODULE - FOURTH MODULE CORRECT";
     }
 }
-
