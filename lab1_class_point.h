44ed5a788c3c8f4279ca9b704ac239f3dbbf2550
say1dead (Сергей) <kochurov.ko4urov@yandex.ru>
2023-02-21 18:03:02 +0300

    класс для координат

---
+++ lab1_class_point.h
@@ -0,0 +1,14 @@
+//
+// Created by kochu on 20.02.2023.
+//
+
+#ifndef UNTITLED28_LAB1_CLASS_POINT_H
+#define UNTITLED28_LAB1_CLASS_POINT_H
+class Point{
+public:
+    int x1;
+    int y1;
+
+    Point(int x1, int y1) : x1(x1), y1(y1) {}
+};
+#endif //UNTITLED28_LAB1_CLASS_POINT_H
